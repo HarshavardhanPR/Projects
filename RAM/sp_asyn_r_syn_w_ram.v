7485cf3c87c532a6601e5e0ea78776e74feae551
HarshavardhanPR <165755963+HarshavardhanPR@users.noreply.github.com>
2026-04-16 19:43:01 +0530

    Add Single-port asynchronous read and synchronous write RAM module

---
+++ RAM/sp_asyn_r_syn_w_ram.v
@@ -0,0 +1,28 @@
+module sp_asyn_r_syn_w_ram 
+  #(parameter data_width = 8, 
+    parameter address_width = 8, 
+    parameter depth = 1 << address_width) 
+  
+  (input clk, cs, we, oe, 
+   input [address_width-1:0] address, 
+   inout [data_width-1:0] data);
+
+reg [data_width-1:0] data_out;
+reg [data_width-1:0] memory [0:depth-1];
+
+assign data = (cs && oe && !we)? data_out : 8'hz;
+
+always@(posedge clk) begin : write_block
+  if(cs && we && !oe) begin
+		memory [address] <= data;
+	end
+end
+
+
+always@(address or cs or we or oe )begin : read_block
+	if(cs && oe && !we) begin
+		data_out <= memory[address];
+	end
+end
+
+endmodule
